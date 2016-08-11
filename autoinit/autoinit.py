f6e47bb8f4c8775967f6cff5e287cc4eea757f93
Luciano Simon <lucianosimon399@yahoo.com>
2016-08-11 21:21:20 +0800

    improve output of exception

--- autoinit/autoinit.py
+++ autoinit/autoinit.py
@@ -1,12 +1,27 @@
 #!/usr/bin/env python3
 #-*- coding:utf-8 -*-
 
-import settings
-import pexpect
+import time
+from tools import bcolors
+from os.path import join
+from os.path import exists
+import subprocess
+from settings import SOFTWARES
+from settings import BASE_DIR
 
 if __name__ == '__main__':
-    softwares = settings.SOFTWARES
-    for s in softwares:
-        print("init %s" % s)
-        pexpect.run(r"python3 %s_conf.py" % s)
-        print("%s had been initialized." % s)
+    for sft in SOFTWARES:
+        print("---------------------------------")
+        print("init %s" % sft)
+        sft_path = join(BASE_DIR, "autoinit/%s_conf.py" % sft)
+        try:
+            if not exists(sft_path):
+                raise FileNotFoundError("[Error]%s:No such file." % sft_path)
+            child = subprocess.Popen(["python3", sft_path])
+            child.wait()
+        except FileNotFoundError as e:
+            print(bcolors.WARNING + e.__str__() + bcolors.ENDC)
+        else:
+            print("%s had been initialized." % sft)
+        print("---------------------------------")
+        time.sleep(1)
--- autoinit/tools.py
+++ autoinit/tools.py
@@ -9,3 +9,13 @@ def is_installed(sft):
         return True
     else:
         raise Exception("You really should have a look. There is something really wrong in tools.py")
+
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[31m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
